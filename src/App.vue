93449f51aa30e0703661bcde01d325e4e988df77
Tu <tucq88@gmail.com>
2019-03-15 00:08:39 +0700

    fix: update structure

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Login</router-link> |
+      <router-link to="/about">Requiring Logged In</router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <form action="">
+    <div>
+      <label for="email">Email</label>
+      <input type="email" placeholder="Your email" id="email" v-model="email">
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input type="passowrd" placeholder="Your secret password" id="password" v-model="password">
+    </div>
+    <div>
+      <button type="button" v-on:click="login()">Login</button>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class LoginForm extends Vue {
+  @Prop({default: ''}) public email!: string;
+  @Prop({default: ''}) public password!: string;
+
+  public login() {
+    console.log(this.email, this.password);
+  }
+
+}
+</script>
+
+<style scoped lang="scss">
+</style>
